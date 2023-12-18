7b3e09ebdd594355050122e69b6231cd273e4a15
Ubuntu <ubuntu@ip-172-31-34-255.us-east-2.compute.internal>
2023-12-18 19:33:43 +0000

    Primera Fase Unidad 1

---
+++ PruebaAudioVisual.java
@@ -0,0 +1,16 @@
+/**
+ * Class PruebaAudioVisual
+ */
+import uni1a.*;
+
+public class PruebaAudioVisual {
+	public static void main(String[] args) {
+    	System.out.println("Hello from AWS Cloud9!");
+        ContenidoAudiovisual av = new ContenidoAudiovisual("Avatar", 125, "Accion");
+        ContenidoAudiovisual deeter = new ContenidoAudiovisual("De aqui a la Eternindad", 90, "accion");
+        av.titulo = "De aqui a la Eternidad";
+        //av.duracionEnMinutos = 140;
+        //av.genero = "Guerra";
+	}
+	
+}
---
+++ uni1a/ContenidoAudiovisual.java
@@ -0,0 +1,17 @@
+/* **
+ * Class ContenidoAudiovisual
+ */
+package uni1a;
+
+public class ContenidoAudiovisual {
+    public String titulo;
+    protected int duracionEnMinutos;
+    private String genero;
+    
+    public ContenidoAudiovisual(String titulo, int duracionEnMinutos, String genero) {
+        this.titulo = titulo;
+        this.duracionEnMinutos = duracionEnMinutos;
+        this.genero = genero;
+    }
+
+}
